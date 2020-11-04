04eef13a58ed67a51ff9e0cac05736e4cc499fd5
Chandan AM <chandanarni@gmail.com>
2020-11-04 15:04:18 +0000

    week1

--- Week 1/operations.sh
+++ Week 1/operations.sh
@@ -1,16 +1,45 @@
 #! /bin/sh
 
-echo "Enter first number"
+echo "Enter two numbers"
 read a
-echo "Enter second number"
 read b
-sum=`echo $a + $b|bc`
-product=`echo $a \*$b|bc`
-difference=`echo $a - $b|bc`
-mod=`echo $a%$b|bc`
-quotient=`echo $a/$b|bc`
-echo "Sum is  "$sum
-echo "Product is  "$product
-echo "Difference is  "$difference
-echo "Mod is  "$mod
-echo "Quotient is  "$quotient
+
+echo "1:Add 2:Subtract 3:Multiply 4:Divide 5:Find remainder"
+read ch
+case $ch in
+  1)res=`echo $a + $b | bc` 
+  ;; 
+  2)res=`echo $a - $b | bc` 
+  ;; 
+  3)res=`echo $a \* $b | bc` 
+  ;; 
+  4)res=`echo $a / $b | bc` 
+  ;; 
+  5)res=`echo $a \% $b | bc`
+  ;;
+esac
+echo "Result : $res"
+
+
+
+# Enter two numbers
+# 19
+# 3
+# 1:Add 2:Subtract 3:Multiply 4:Divide 5:Find remainder
+# 5
+# Result : 1
+
+
+# Enter two numbers
+# 36
+# 11
+# 1:Add 2:Subtract 3:Multiply 4:Divide 5:Find remainder
+# 2
+# Result : 25
+
+# Enter two numbers
+# 2
+# 4
+# 1:Add 2:Subtract 3:Multiply 4:Divide 5:Find remainder
+# 3
+# Result : 8
